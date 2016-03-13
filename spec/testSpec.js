b61476a53a4574b9397422c16ba7257641f56d2f
Alex Harris <chasenyc@gmail.com>
2016-03-13 13:37:00 -0400

    Add basic functionality.

---
+++ spec/testSpec.js
@@ -0,0 +1,27 @@
+it("Sum", function() {
+  should("handle simple addition", function() {
+    expect(sum(1,2)).toEqual(3);
+    expect(sum(2,2)).toNotEqual(5);
+    expect(sum(2,2)).toEqual(4);
+  });
+  should("should handle negative numbers", function() {
+    expect(sum(1,-1)).toEqual(0)
+    expect(sum(-3,-1)).toEqual(-4)
+    expect(sum(-3,-1)).toNotEqual(-4)
+  });
+  should("should handle one element", function() {
+    expect(sumo(1)).toEqual(1)
+  });
+});
+
+
+it("Reverse", function() {
+  should( "handles small string", function() {
+    expect(reverse('alex')).toEqual('xela');
+    expect(reverse('aacd')).toEqual('dcaa');
+    expect(reverse('xax')).toNotEqual('axa');
+  });
+  should("takes an empty string", function() {
+    expect(reverse('')).toEqual('');
+  });
+});
---
+++ src/test.js
@@ -0,0 +1,9 @@
+var sum = function(numOne, numTwo) {
+  numOne = numOne || 0;
+  numTwo = numTwo || 0;
+  return numOne + numTwo
+}
+
+var reverse = function(s) {
+  return s.split("").reverse().join("");
+}
